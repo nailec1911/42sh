8e20714ce37d30694f832f565f5947fea82e8404
celian <celian.lombardot@epitech.eu>
2023-04-10 17:25:52 +0200

    [FIX] priority order between '||' and '&&'
    for '&&' the right part can be executed only if the last command was executed

--- src/exec_command/go_throught_grocommand.c
+++ src/exec_command/go_throught_grocommand.c
@@ -9,15 +9,15 @@
 #include "macro_errors.h"
 #include "exec_command.h"
 
-static bool do_exec(all_args_t *all_args, int i)
+static bool do_exec(all_args_t *all_args, int i, bool prev_executed)
 {
     if (all_args->ast.tab_grocommands[i].type == SEMICOLON)
         return true;
     if (all_args->ast.tab_grocommands[i].type == OPERATOR_OR
-    && all_args->to_return != 0)
+    && all_args->last_status != 0)
         return true;
     if (all_args->ast.tab_grocommands[i].type == OPERATOR_AND
-    && all_args->to_return == 0)
+    && all_args->last_status == 0 && prev_executed)
         return true;
     return false;
 }
@@ -26,11 +26,16 @@ int go_throught_grocommand(all_args_t *all_args)
 {
     int res = 0;
     int exit = 0;
+    bool prev_executed = false;
+
     for (int i = 0; i < all_args->ast.nb_grocommand; i += 1) {
-        if (do_exec(all_args, i)) {
-        all_args->to_return = all_args->last_status;
-        all_args->last_status = 0;
+        if (do_exec(all_args, i, prev_executed)) {
+            all_args->to_return = all_args->last_status;
+            all_args->last_status = 0;
             res = exec_grocommand(all_args, all_args->ast.tab_grocommands[i]);
+            prev_executed = true;
+        } else {
+            prev_executed = false;
         }
         if (res == ERROR)
             return ERROR;
