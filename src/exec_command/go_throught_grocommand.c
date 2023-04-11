0fb3be683e520426a5c227c5c1427600ce0f4e55
Zetsumei <theophile.combes@epitech.eu>
2023-04-11 17:38:39 +0200

    Revert "[FIX] priority order between '||' and '&&'"
    This reverts commit c853ee8c0324b04af729bd20b366ab13346dc9b7.

--- src/exec_command/go_throught_grocommand.c
+++ src/exec_command/go_throught_grocommand.c
@@ -9,15 +9,15 @@
 #include "macro_errors.h"
 #include "exec_command.h"
 
-static bool do_exec(all_args_t *all_args, int i, bool prev_executed)
+static bool do_exec(all_args_t *all_args, int i)
 {
     if (all_args->ast.tab_grocommands[i].type == SEMICOLON)
         return true;
     if (all_args->ast.tab_grocommands[i].type == OPERATOR_OR
-    && all_args->last_status != 0)
+    && all_args->to_return != 0)
         return true;
     if (all_args->ast.tab_grocommands[i].type == OPERATOR_AND
-    && all_args->last_status == 0 && prev_executed)
+    && all_args->to_return == 0)
         return true;
     return false;
 }
@@ -26,16 +26,11 @@ int go_throught_grocommand(all_args_t *all_args)
 {
     int res = 0;
     int exit = 0;
-    bool prev_executed = false;
-
     for (int i = 0; i < all_args->ast.nb_grocommand; i += 1) {
-        if (do_exec(all_args, i, prev_executed)) {
-            all_args->to_return = all_args->last_status;
-            all_args->last_status = 0;
+        if (do_exec(all_args, i)) {
+        all_args->to_return = all_args->last_status;
+        all_args->last_status = 0;
             res = exec_grocommand(all_args, all_args->ast.tab_grocommands[i]);
-            prev_executed = true;
-        } else {
-            prev_executed = false;
         }
         if (res == ERROR)
             return ERROR;
