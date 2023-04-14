544b027b34e14b15dd6c016b9a65231d029a2b01
celian <celian.lombardot@epitech.eu>
2023-04-14 16:14:25 +0200

    [FIX] return value with '||' and '&&', only the last executed is returned

--- src/exec_command/go_through_grocommand.c
+++ src/exec_command/go_through_grocommand.c
@@ -14,16 +14,18 @@ static int go_through_and_command(mysh_t *mysh, or_command_t *or_command)
 {
     int res = 0;
     int exit = 0;
+    int i = 0;
 
-    for (int i = 0; i < or_command->nb_command; i += 1) {
-        mysh->to_return = mysh->last_status;
+    do {
         mysh->last_status = 0;
         res = exec_and_command(mysh, &(or_command->tab_command[i]));
         if (res == ERROR)
             return ERROR;
         if (res == EXIT)
             exit = EXIT;
-    }
+        i += 1;
+    } while (i < or_command->nb_command && mysh->last_status == SUCCESS);
+
     return exit;
 }
 
@@ -31,16 +33,18 @@ static int go_through_or_command(mysh_t *mysh, grocommand_t *grocommand)
 {
     int res = 0;
     int exit = 0;
+    int i = 0;
 
-    for (int i = 0; i < grocommand->nb_command; i += 1) {
-        mysh->to_return = mysh->last_status;
-        mysh->last_status = 0;
+    do {
+        mysh->last_status = SUCCESS;
         res = go_through_and_command(mysh, &(grocommand->tab_command[i]));
         if (res == ERROR)
             return ERROR;
         if (res == EXIT)
             exit = EXIT;
-    }
+        i += 1;
+    } while ( i < grocommand->nb_command && mysh->last_status != SUCCESS);
+
     return exit;
 }
 
@@ -49,8 +53,9 @@ int go_through_grocommand(mysh_t *mysh)
     int res = 0;
     int exit = 0;
 
-    for (int i = 0; i < mysh->ast.nb_grocommand; i += 1) {
+    if (mysh->last_status != SUCCESS)
         mysh->to_return = mysh->last_status;
+    for (int i = 0; i < mysh->ast.nb_grocommand; i += 1) {
         mysh->last_status = 0;
         res = go_through_or_command(mysh, &(mysh->ast.tab_grocommands[i]));
         if (res == ERROR)
@@ -58,5 +63,6 @@ int go_through_grocommand(mysh_t *mysh)
         if (res == EXIT)
             exit = EXIT;
     }
+
     return exit;
 }
