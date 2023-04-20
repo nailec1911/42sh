988683a29d41fc901e7980de6d663e2674adcbdc
celian <celian.lombardot@epitech.eu>
2023-04-20 05:08:42 +0200

    [ADD] handling of alias on lexer
    if there is an alias, it's token after lexing are inserted at the place of it's identifier

--- src/handle_input/handle_input.c
+++ src/handle_input/handle_input.c
@@ -5,18 +5,25 @@
 ** handle_input
 */
 
+#include <string.h>
+#include <stdlib.h>
 #include "parser/parse.h"
 #include "mysh.h"
 #include "macro_errors.h"
 int tokens_to_history(mysh_t *mysh, token_t *list);
+token_t *loop_for_aliases(mysh_t *mysh, token_t *list);
+
 
 int handle_input(mysh_t *mysh, char *input)
 {
     token_t *list_token = lexer(input);
 
+    if (list_token == NULL)
+        return ERROR;
     if (tokens_to_history(mysh, list_token) == ERROR)
         return ERROR;
-
+    if ((list_token = loop_for_aliases(mysh, list_token)) == NULL)
+        return ERROR;
     free(input);
     return SUCCESS;
 }
