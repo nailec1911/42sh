59f1f88a3cca569cdbd0d9664a01cf3abd68d887
celian <celian.lombardot@epitech.eu>
2023-04-20 06:09:49 +0200

    Revert "[CLEAN-CODE] better include"
    This reverts commit 88c37083f7a92aeaa3358de2e11b497e94638417.

--- src/handle_input/handle_input.c
+++ src/handle_input/handle_input.c
@@ -8,9 +8,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "parser/parse.h"
-#include "handle_input.h"
-#include "macro_errors.h"
 #include "mysh.h"
+#include "macro_errors.h"
+int tokens_to_history(mysh_t *mysh, token_t *list);
+token_t *loop_for_aliases(mysh_t *mysh, token_t *list);
 
 static token_t *get_list_tokens(mysh_t *mysh, char *input)
 {
