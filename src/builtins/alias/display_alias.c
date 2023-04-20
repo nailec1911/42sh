f879f8a4e025f9593104ae0893d806db7f12bd08
celian <celian.lombardot@epitech.eu>
2023-04-20 16:35:54 +0200

    [FIX] display of builtins history and alias is made in good fd_out

--- src/builtins/alias/display_alias.c
+++ src/builtins/alias/display_alias.c
@@ -10,42 +10,45 @@
 #include "macro_errors.h"
 #include "str_func.h"
 
-int display_alias(mysh_t *mysh)
+int display_alias(alias_t alias, int fd)
 {
-    int l_tab = length_tab(mysh->alias.tab_file) - 1;
+    int l_tab = length_tab(alias.tab_file) - 1;
 
     for (; l_tab >= 0; l_tab -= 1) {
-        printf("%s", mysh->alias.tab_file[l_tab]);
+        dprintf(fd, "%s", alias.tab_file[l_tab]);
     }
     return SUCCESS;
 }
 
-static int search_specific_alias(mysh_t *mysh, int l_tab, char *to_find)
+static int search_specific_alias(char *line, char *to_find, int fd)
 {
     char *to_display = NULL;
-    char **tab_alias = my_str_to_word_array_separator
-    (mysh->alias.tab_file[l_tab], " \n");
+    char **tab_alias = my_str_to_word_array_separator(line, " \n");
     if (tab_alias == NULL || tab_alias[2] == NULL)
         return ERROR;
     to_display = remake_command(tab_alias);
     if (to_display == NULL)
         return ERROR;
     if (strcmp(tab_alias[1], to_find) == 0) {
-        printf("%s\n", to_display);
+        dprintf(fd, "%s\n", to_display);
         free(to_display);
         free_array(tab_alias);
-        return SUCCESS;
+        return EXIT;
     }
     free(to_display);
     free_array(tab_alias);
     return SUCCESS;
 }
 
-int display_specific_alias(mysh_t *mysh, char *to_find)
+int display_specific_alias(alias_t alias, char *to_find, int fd)
 {
-    int l_tab = length_tab(mysh->alias.tab_file) - 1;
-    for (;l_tab >= 0; l_tab -= 1) {
-        return search_specific_alias(mysh, l_tab, to_find);
+    int l_tab = length_tab(alias.tab_file) - 1;
+    int res = SUCCESS;
+
+    for (;l_tab >= 0 && res == SUCCESS; l_tab -= 1) {
+        res = search_specific_alias(alias.tab_file[l_tab], to_find, fd);
+        if (res == ERROR)
+            return ERROR;
     }
     return SUCCESS;
 }
