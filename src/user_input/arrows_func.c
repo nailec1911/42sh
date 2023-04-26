e047f024ccde26e35cc4428d23c387d7f5e5c9d4
De lacheisserie Levy Adam <adam.de-lacheisserie-levy@epitech.eu>
2023-04-26 18:08:16 +0200

    [ADD] macros to understand the user inputs

--- src/user_input/arrows_func.c
+++ src/user_input/arrows_func.c
@@ -10,17 +10,18 @@
 #include <stdlib.h>
 #include "mysh.h"
 #include "str_func.h"
+#include "macros_inputs.h"
 char *suppr_function(int *index, int *length, char *line);
 void choose_arrow(mysh_t *mysh, int *index, int *length, char **line);
 
 void arrow_function(int *index, int *length, char **line, mysh_t *mysh)
 {
     read(STDIN_FILENO, &mysh->ch, 1);
-    if (mysh->ch == 91) {
+    if (mysh->ch == BRACKET) {
         read(STDIN_FILENO, &mysh->ch, 1);
         choose_arrow(mysh, index, length, line);
     }
-    if (mysh->ch == 51) {
+    if (mysh->ch == '3') {
         read(STDIN_FILENO, &mysh->ch, 1);
         *line = suppr_function(index, length, *line);
     }
