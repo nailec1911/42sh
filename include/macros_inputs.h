56ef0e6fb68fe2bc110605528f69a93f3f491096
Adam <adam.de-lacheisserie-levy@epitech.eu>
2023-04-26 16:13:08 +0000

    Line edition

---
+++ include/macros_inputs.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** macros_inputs
+*/
+
+#ifndef MACROS_INPUTS_H_
+    #define MACROS_INPUTS_H_
+
+    #define ENTER 10
+    #define ESC 27
+    #define CTRL_D 4
+    #define DELETE 127
+    #define BRACKET 91
+
+#endif /* !MACROS_INPUTS_H_ */
