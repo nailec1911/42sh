25ad5d2f76626d7441c63c098b7669309faab5c2
Ciikorrr <marius.marolleau@epitech.eu>
2023-05-03 16:11:14 +0200

    [ADD] return value on prompt

--- src/user_input/prompt.c
+++ src/user_input/prompt.c
@@ -11,25 +11,42 @@
 #include "mysh.h"
 #include "builtins/env.h"
 
-int init_prompt(mysh_t *mysh)
+char **set_all_for_prompt(char **user, char **pwd, int *tab_l, mysh_t *mysh)
 {
     struct passwd *info;
-    int tab_l = 0;
     char **tab = NULL;
-    char *user = get_env_var(mysh, "USER=");
-    char *pwd = get_env_var(mysh, "PWD=");
-    if (pwd == NULL)
-        pwd = getcwd(pwd, 0);
-    tab = my_str_to_word_array_separator(pwd, "/");
+    *user = get_env_var(mysh, "USER=");
+    *pwd = get_env_var(mysh, "PWD=");
+    if (*pwd == NULL)
+        *pwd = getcwd(*pwd, 0);
+    tab = my_str_to_word_array_separator(*pwd, "/");
     if (tab == NULL)
-        return ERROR;
-    tab_l = length_tab(tab);
+        return NULL;
+    *tab_l = length_tab(tab);
     if (user == NULL) {
         info = getpwuid(getuid());
-        user = info->pw_name;
+        *user = info->pw_name;
     }
-    printf("\33[34;01m%s\33[00m | \33[32;01m%s\33[00m > ",
-    tab[tab_l - 1], user);
+    return tab;
+}
+
+int init_prompt(mysh_t *mysh)
+{
+    if (!mysh->tty)
+        return SUCCESS;
+    char *pwd = NULL;
+    char *user = NULL;
+    int tab_l = 0;
+    char **tab = NULL;
+    if ((tab = set_all_for_prompt(&user, &pwd, &tab_l, mysh)) == NULL)
+        return ERROR;
+    if (mysh->last_status == 0) {
+        printf("\33[37;01m%d\33[00m | \33[34;01m%s\33[00m | \
+\33[32;01m%s\33[00m > ", mysh->last_status, tab[tab_l - 1], user);
+    } else {
+        printf("\33[31;01m%d\33[00m | \33[34;01m%s\33[00m | \
+\33[32;01m%s\33[00m > ", mysh->last_status, tab[tab_l - 1], user);
+    }
     fflush(stdout);
     free_array(tab);
     return SUCCESS;
