9f41cbcfdb6792c83de43aa88e1b8418397e426b
celian <celian.lombardot@epitech.eu>
2023-05-09 20:45:15 +0200

    [CLEAN-CODE] add check of pointer and coding style on vars

--- src/builtins/vars/recreate_args.c
+++ src/builtins/vars/recreate_args.c
@@ -9,24 +9,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static int count_buf_len(char **args, int idx)
+{
+    int buf_len = strlen(args[idx]) + 1 + 1;
+    buf_len += args[idx + 1] && args[idx + 2] ? strlen(args[idx + 2]) : 0;
+    return buf_len;
+}
+
+static void append_vars(char **args, char **new_args, int new_idx, int idx)
+{
+    if (!new_args[new_idx])
+        return;
+    sprintf(new_args[new_idx], "%s%s%s", args[idx], args[idx + 1],
+                    args[idx + 1] && args[idx + 2] ? args[idx + 2] : "");
+}
+
 char **recreate_args(char **args)
 {
     char **new_args = calloc(1, sizeof(char*));
-    int idx = 0, new_idx = 0;
+    int idx = 0;
+    int new_idx = 0;
 
-    if (!new_args)
-        return 0;
-    for (; args[idx] != 0; new_idx++) {
-        char *next_arg = args[idx + 1];
-        if (next_arg && next_arg[0] == '=' && next_arg[1] == 0) {
-            int buf_len = strlen(args[idx]) + 1 + 1;
-            buf_len += next_arg && args[idx + 2] ? strlen(args[idx + 2]) : 0;
-            new_args[new_idx] = calloc(buf_len, sizeof(char));
-            sprintf(new_args[new_idx], "%s%s%s", args[idx], next_arg,
-                    next_arg && args[idx + 2] ? args[idx + 2] : "");
-            idx += next_arg && args[idx + 2] ? 3 : 2;
+    for (; args && args[idx] != 0; new_idx++) {
+        if (!new_args)
+            return 0;
+        if (args[idx + 1] && args[idx + 1][0] == '=' && args[idx + 1][1] == 0) {
+            new_args[new_idx] = calloc(count_buf_len(args, idx), sizeof(char));
+            append_vars(args, new_args, new_idx, idx);
+            idx += args[idx + 1] && args[idx + 2] ? 3 : 2;
         } else
             new_args[new_idx] = strdup(args[idx++]);
+        if (!new_args[new_idx])
+            return 0;
         new_args = realloc(new_args, sizeof(char*) * (new_idx + 2));
     }
     new_args[new_idx] = 0;
