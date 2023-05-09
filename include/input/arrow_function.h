8fb935582e60a4165bed78b2772679d6f6b37c1c
De lacheisserie Levy Adam <adam.de-lacheisserie-levy@epitech.eu>
2023-05-09 13:50:30 +0200

    [ADD] input folder to stock every '.h' files regarding the users_input files

---
+++ include/input/arrow_function.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** arrow_function
+*/
+
+#ifndef ARROW_FUNCTION_H_
+    #define ARROW_FUNCTION_H_
+
+    #include "mysh.h"
+
+    char *remove_line(int res, int *length, int *index, char *line);
+    void ctrl_functions(int *index, int *length, int ch);
+
+#endif /* !ARROW_FUNCTION_H_ */
