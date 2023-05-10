00468b51a59a5781c9be55470c51581d9c6b5175
Zetsumei <theophile.combes@epitech.eu>
2023-05-10 16:54:03 +0200

    [FIX] coding style

--- src/exec_command/job_and_process_methods/remove_job_from_list.c
+++ src/exec_command/job_and_process_methods/remove_job_from_list.c
@@ -44,7 +44,6 @@ job_list *remove_job_from_list(job_list *list, int job_id)
 {
     int position = 0;
     job_list *tmp = list;
-
     if (job_id < 0 || !list)
         return list;
     if ((position = get_job_pos(list, job_id)) < 0)
