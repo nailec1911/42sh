9f2965726167b1731b4fce9393957fc33ba46aaa
celian <celian.lombardot@epitech.eu>
2023-05-11 17:18:58 +0200

    [CLEAN-CODE] remove unused files

--- include/str_func.h
+++ include/str_func.h
@@ -19,11 +19,8 @@
     int is_in(char c, char *str);
     int my_strstrlen(char *const str[]);
     char *clean_last_input(char **tab_alias);
-    char *remove_first_char(char *input);
     bool is_num_char(char *str, char c);
     char **my_str_to_word_array(char *str, char const *separator);
     int count_word(char const *str, char const *separator);
-    char *read_file(const char *filename);
-    char *read_file_from_stream(FILE *stream);
 
 #endif /* !STR_FUNC_H_ */
